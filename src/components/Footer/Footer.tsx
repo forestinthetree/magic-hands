ef9e5be4f5604db625415ff81700da7d44dd077e
Tak Tran <contact@tutaktran.com>
2023-03-13 22:49:59 +0000

    Update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,15 +5,8 @@ import styles from "./Footer.module.scss";
 export const Footer: Component = () => {
   return (
     <footer class={styles.footer}>
-      A remix of human crafted{" "}
-      <a href="https://paveldogreat.github.io/WebGL-Fluid-Simulation/">
-        fluid simulation
-      </a>{" "}
-      and machine crafted{" "}
-      <a href="https://mediapipe-studio.webapps.google.com/demo/gesture_recognizer">
-        hand detection
-      </a>
-      , by <a href="https://www.forestinthetree.com/">Forest in the Tree</a>
+      Made with <i class="ph ph-heart"></i> by{" "}
+      <a href="https://www.forestinthetree.com/">Forest in the Tree</a>
     </footer>
   );
 };
